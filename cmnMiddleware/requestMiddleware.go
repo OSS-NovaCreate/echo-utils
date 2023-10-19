8317127fff27e492ef96db630b9562f9e397af5e
Nakatsuka <burakku0623@gmail.com>
2023-10-20 00:28:56 +0900

    同時リクエスト対策

--- cmnMiddleware/requestMiddleware.go
+++ cmnMiddleware/requestMiddleware.go
@@ -5,34 +5,19 @@ import (
 
 	"github.com/OSS-NovaCreate/echo-utils/database"
 	"github.com/OSS-NovaCreate/echo-utils/utils/errorUtil"
-	"github.com/OSS-NovaCreate/echo-utils/utils/requestUtil"
 	"github.com/labstack/echo/v4"
 )
 
-func Request(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		requestUtil.SetContext(c)
-		requestUtil.SetUserAgent(c.Request().UserAgent())
-		next(c)
-		return nil
-	}
-}
-
 // DBコネクションとトランザクションを開始する
-func ConnectionStart(next echo.HandlerFunc) echo.HandlerFunc {
+func TransactionStart(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		//DBとコネクションする
-		err := database.Open()
-		if err != nil {
-			return internalServerError(errorUtil.Error(err.Error()).Error())
-		}
-
 		//トランザクション開始
+		var err error
 		database.DbTx, err = database.Database.Beginx()
 		if err != nil {
 			e := errorUtil.Error(err.Error())
 			errorUtil.WriteErrorLog(e)
-			return internalServerError(e.Error())
+			return internalServerError(c, e.Error())
 		}
 
 		next(c)
@@ -45,7 +30,6 @@ func ConnectionEnd(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		next(c)
 		database.DbTx.Commit()
-		database.Database.Close()
 
 		return nil
 	}
@@ -56,7 +40,7 @@ type errorResponse struct {
 }
 
 // 意図しないエラーのレスポンス
-func internalServerError(log string) error {
+func internalServerError(c echo.Context, log string) error {
 
 	//エラーオブジェクト作成
 	errObj := errorResponse{
@@ -65,9 +49,6 @@ func internalServerError(log string) error {
 
 	//既存のトランザクションをロールバック
 	database.DbTx.Rollback()
-
-	//コンテキストを取得
-	c := requestUtil.GetContext()
 
 	//エラーレスポンス
 	return c.JSON(http.StatusInternalServerError, errObj)
